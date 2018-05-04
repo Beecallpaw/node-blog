ce777ce12d136ca68cab13f64a03ac0f7f995219
Beecallpaw <thapabeecallpaw@gmail.com>
2018-05-04 13:32:09 +0545

    created a boilerplate

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express"),
+  app = express(),
+  BP = require("body-parser"),
+  mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/my_blog");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(BP.urlencoded({ extended: true }));
+
+let blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  createdDate: { type: Date, default: Date.now }
+});
+let Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) console.log(`Error: ${err}`);
+    res.render("index", { blogs });
+  });
+});
+
+app.listen(3030, () => {
+  console.log("Listening at port 3030");
+});
